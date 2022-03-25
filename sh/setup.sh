bbfe5da0e6426ea87606f8ac5a5599b5c201631f
Conner Harkness <caharkness@gmail.com>
2022-03-25 15:00:09 -0600

    Update setup.sh

--- sh/setup.sh
+++ sh/setup.sh
@@ -6,11 +6,6 @@ source backup-mysql.sh
 source backup-www.sh
 source initialize-apache.sh
 source initialize-php.sh
-source list-sites.sh
-source install-sites.sh
 source deploy-all.sh
 
-chown -R www-data:www-data "$WEB_ROOT"
-chmod -R 0755 "$WEB_ROOT"
-
-service apache2 restart
+update_permissions

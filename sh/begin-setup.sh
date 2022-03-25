2b07c4c85c880e9b0bf7de45defa86ae87749e25
Conner Harkness <caharkness@gmail.com>
2022-03-25 15:00:50 -0600

    Update begin-setup.sh

--- sh/begin-setup.sh
+++ sh/begin-setup.sh
@@ -3,12 +3,4 @@
 source common.sh
 about_this "This script should be the very first thing you run (once) after installing and/or cloning apache-site-manager to your filesystem."
 
-read_carefully "Review the file located at apache-site-manager/sh/config.sh and make any necessary changes before proceeding."
-read_carefully "You must install Apache for this tool to do its job. Proceed when apache2 is installed."
-
-warn_user "Install all the dependencies using apt"
-
-if [[ $DO_SCRIPT -eq 1 ]];
-then
-    apt install apache2 php7.4
-fi
+source setup.sh
